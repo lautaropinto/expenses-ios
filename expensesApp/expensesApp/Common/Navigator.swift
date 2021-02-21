86944815826be16600dfcdcded37092b66f38242
Lautaro Pinto <lautaro.pinto@naranjax.com>
2021-02-21 16:15:30 -0300

    Se agrega la clase Navigator.

---
+++ expensesApp/expensesApp/Common/Navigator.swift
@@ -0,0 +1,32 @@
+//
+//  Navigator.swift
+//  expensesApp
+//
+//  Created by Lautaro Pinto on 21/02/2021.
+//
+
+import UIKit
+
+internal class Navigator: NavigatorProtocol {
+    public var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func push(_ viewController: UIViewController, animated: Bool) {
+        navigationController.pushViewController(viewController, animated: animated)
+    }
+    
+    func popToRoot(animated: Bool) {
+        navigationController.popToRootViewController(animated: animated)
+    }
+    
+    func pop(animated: Bool) {
+        navigationController.popViewController(animated: animated)
+    }
+    
+    func setRoot(_ viewController: UIViewController) {
+        navigationController.setViewControllers([viewController], animated: false)
+    }
+}
