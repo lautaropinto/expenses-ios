babe4ef7707be5174a6e67f654c2bebccd5c3c81
Lautaro Pinto <pintolautaro@outlook.com>
2021-07-13 20:55:15 -0300

    HomeScene was refactored.

---
+++ expensesApp/expensesApp/Scenes/MainView/HomeLayoutable.swift
@@ -0,0 +1,52 @@
+//
+//  HomeLayoutable.swift
+//  expensesApp
+//
+//  Created by Lautaro Pinto on 13/07/2021.
+//
+
+import UIKit
+
+internal protocol HomeLayoutable: UIViewController, ProgramaticalLayout {
+    var mainView: HomeView { get set }
+    
+    func setGradientBackground()
+}
+
+extension HomeLayoutable {
+    func setUpViewHierarchy() {
+        view.addSubview(mainView)
+    }
+    
+    func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            mainView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            mainView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func setUpAdditionalConfig() {
+        navigationController?.isNavigationBarHidden = true
+        setGradientBackground()
+    }
+    
+    func setGradientBackground() {
+        let colorTop =  Colors.purple01
+        let colorMid = Colors.purple02
+        let colorBottom = Colors.lightPurple
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.type = .axial
+        gradientLayer.colors = [colorTop.withAlphaComponent(0.9).cgColor,
+                                colorMid.withAlphaComponent(0.7).cgColor,
+                                colorBottom.withAlphaComponent(0.2).cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        let frame = CGRect(origin: self.view.frame.origin,
+                           size: CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height*0.4))
+        gradientLayer.frame = frame
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
--- expensesApp/expensesApp/Scenes/MainView/HomeViewController.swift
+++ expensesApp/expensesApp/Scenes/MainView/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-internal final class HomeViewController: UIViewController, ProgramaticalLayout, ExpenseFetchable {
+internal final class HomeViewController: UIViewController, HomeLayoutable, ExpenseFetchable {
     @UsesAutoLayout var mainView = HomeView()
     
     var expenses: [Expense] = [] {
@@ -24,44 +24,7 @@ internal final class HomeViewController: UIViewController, ProgramaticalLayout, 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-
         setUpView()
-    }
-    
-    func setUpViewHierarchy() {
-        view.addSubview(mainView)
-    }
-    
-    func setUpConstraints() {
-        NSLayoutConstraint.activate([
-            mainView.heightAnchor.constraint(equalTo: view.heightAnchor),
-            mainView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
-    
-    func setUpAdditionalConfig() {
-        navigationController?.isNavigationBarHidden = true
-        setGradientBackground()
-    }
-    
-    func setGradientBackground() {
-        let colorTop =  Colors.purple01
-        let colorMid = Colors.purple02
-        let colorBottom = Colors.lightPurple
-                    
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.type = .axial
-        gradientLayer.colors = [colorTop.withAlphaComponent(0.9).cgColor,
-                                colorMid.withAlphaComponent(0.7).cgColor,
-                                colorBottom.withAlphaComponent(0.2).cgColor]
-        gradientLayer.locations = [0.0, 1.0]
-        let frame = CGRect(origin: self.view.frame.origin,
-                           size: CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height*0.4))
-        gradientLayer.frame = frame
-                
-        self.view.layer.insertSublayer(gradientLayer, at:0)
     }
 }
 
